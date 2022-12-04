f974aa231ae1c7bf5fb8fb5f13c52f68fa115552
MabeDiaby <diabymabe@gmail.com>
2022-12-03 22:51:18 -0500

    randome colors

--- practice/turtleWalk.py
+++ practice/turtleWalk.py
@@ -1,10 +1,21 @@
 from turtle import Turtle, Screen
+import turtle as t
 import random
 
 walk = Turtle()
+t.colormode(255)
 walk.shape("turtle")
 
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    colors = (r, g, b)
+    return colors
+
+# python tuples - cant be changed unlike lists ex: (1, 3, 8)
+
+# colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 directions = [0,90,180,270]
 
 walk.speed('fastest')
@@ -12,7 +23,8 @@ walk.pensize(15)
 for walks in range(200):
     walk.forward(30)
     walk.setheading(random.choice(directions))
-    walk.color(random.choice(colors))
+    walk.color(random_color())
+    # walk.color(random.choice(colors))
 
 screen = Screen()
 screen.exitonclick()
