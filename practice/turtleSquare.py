4b8c344cb5987745613d4d75bfa4bcd3dd7d5304
MabeDiaby <diabymabe@gmail.com>
2022-12-03 21:54:54 -0500

    refactor

--- practice/turtleSquare.py
+++ practice/turtleSquare.py
@@ -1,12 +1,12 @@
 from turtle import Turtle, Screen
 
 
-mabe_the_turtle = Turtle()
-mabe_the_turtle.shape("turtle")
-mabe_the_turtle.color("magenta")
+mabe = Turtle()
+mabe.shape("turtle")
+mabe.color("magenta")
 for turns in range(0,4):
-    mabe_the_turtle.forward(100)
-    mabe_the_turtle.right(90)
+    mabe.forward(100)
+    mabe.right(90)
 
 
 screen = Screen()
